217932260a77c6bf1959eb45ead94e21044f4485
Jonathan Bower <jonathan.bower2010@gmail.com>
2015-04-10 14:37:15 -0400

    Moved test into test block
    Moved the tests for LBFs out of main

--- src/lbf.rs
+++ src/lbf.rs
@@ -126,3 +126,72 @@ pub extern "C" fn lbf_close(lbf : *mut bloom_lbf) -> i32 {
     unsafe { drop(&mut *lbf) };
     return 1;
 }
+
+#[cfg(test)]
+mod tests {
+    use bloom::{bloom_filter_header, bloom_bloomfilter, bloom_filter_params, size_for_capacity_prob, ideal_k_num};
+    use bitmap::{bitmap_mode, bloom_bitmap};
+    use filter::BloomFilter;
+    use super::bloom_lbf;
+    
+    fn create_bloom_filter(params : &bloom_filter_params, index : i32) -> bloom_bloomfilter {
+        let mut map : bloom_bitmap = bloom_bitmap::from_filename(format!("map{}.bmp", index).as_slice(), 1000000, true, bitmap_mode::NEW_BITMAP).unwrap();
+        return bloom_bloomfilter::new(map, params.k_num, true);
+    }
+    
+    #[test]
+    fn test() {
+        let mut params : bloom_filter_params = bloom_filter_params::empty();
+        params.capacity = 1000000;
+        params.fp_probability = 0.001;
+
+        size_for_capacity_prob(&mut params).unwrap();
+        ideal_k_num(&mut params).unwrap();
+
+        println!("bytes: {}, k_num: {}", params.bytes, params.k_num);
+
+        let mut filters : Vec<bloom_bloomfilter> = Vec::new();
+
+        let mut map : bloom_bitmap;
+        let mut header : bloom_filter_header;
+        let mut filter : bloom_bloomfilter;
+
+        for i in (0..3) {
+            filters.push(create_bloom_filter(&params, i));
+        }
+
+        let mut lbf : bloom_lbf = bloom_lbf::new(filters);
+
+        let key1 : String = String::from_str("abc");
+        let key2 : String = String::from_str("def");
+        let key3 : String = String::from_str("ghi");
+
+        // add first key
+        assert!(lbf.add(key1.clone()).unwrap() == 1);
+        assert!(lbf.size() == 1);
+        assert!(lbf.contains(&key1).unwrap() == 1);
+        assert!(lbf.contains(&key2).unwrap() == 0);
+        assert!(lbf.contains(&key3).unwrap() == 0);
+
+        // add second key
+        assert!(lbf.add(key1.clone()).unwrap() == 2);
+        assert!(lbf.add(key2.clone()).unwrap() == 1);
+        assert!(lbf.size() == 2);
+        assert!(lbf.contains(&key1).unwrap() == 2);
+        assert!(lbf.contains(&key2).unwrap() == 1);
+        assert!(lbf.contains(&key3).unwrap() == 0);
+
+        // add third key
+        assert!(lbf.add(key1.clone()).unwrap() == 3);
+        assert!(lbf.add(key2.clone()).unwrap() == 2);
+        assert!(lbf.add(key3.clone()).unwrap() == 1);
+        assert!(lbf.size() == 3);
+        assert!(lbf.contains(&key1).unwrap() == 3);
+        assert!(lbf.contains(&key2).unwrap() == 2);
+        assert!(lbf.contains(&key3).unwrap() == 1);
+
+        assert!(lbf.size() == 3);
+
+        lbf.flush().unwrap();
+    }
+}
